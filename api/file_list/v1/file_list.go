ecaca2e9e85a8dddd999c661f8d5df75b5c3112d
Eadweard <aurora.coder.zqf@gmail.com>
2025-05-20 15:44:11 +0800

    feat: 列表接口

--- api/file_list/v1/file_list.go
+++ api/file_list/v1/file_list.go
@@ -7,7 +7,10 @@ import (
 )
 
 type FileListReq struct {
-	g.Meta `path:"/file_list" method:"get" tags:"FileList" summary:"Get file list"`
+	g.Meta   `path:"/file_list" method:"get" tags:"FileList" summary:"Get file list"`
+	TagId    string
+	Page     int `v:"min:1" dc:"page"`
+	PageSize int `v:"min:1" dc:"page size"`
 }
 type FileListRes struct {
 	List []*entity.FileList `json:"list" dc:"file list"`
--- internal/controller/file_list/file_list_v1_file_list.go
+++ internal/controller/file_list/file_list_v1_file_list.go
@@ -3,12 +3,16 @@ package file_list
 import (
 	"context"
 
-	"github.com/gogf/gf/v2/errors/gcode"
-	"github.com/gogf/gf/v2/errors/gerror"
-
 	v1 "aurora-file-sharing/api/file_list/v1"
+	"aurora-file-sharing/internal/dao"
+	"aurora-file-sharing/internal/model/do"
 )
 
 func (c *ControllerV1) FileList(ctx context.Context, req *v1.FileListReq) (res *v1.FileListRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+	res = &v1.FileListRes{}
+	err = dao.FileList.Ctx(ctx).Where(
+		do.FileList{
+			TagId: req.TagId,
+		}).Page(req.Page, req.PageSize).Order("created_at", "desc").ScanList(&res.List, "id")
+	return
 }
