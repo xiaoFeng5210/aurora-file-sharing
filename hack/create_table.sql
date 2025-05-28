bd0c426893282e5b546e87aee0ad27c4c15bf6b3
Eadweard <aurora.coder.zqf@gmail.com>
2025-05-28 13:11:11 +0800

    feat: sql

--- hack/create_table.sql
+++ hack/create_table.sql
@@ -1,3 +1,5 @@
+-- 进入postgres
+psql -U zhangqingfeng -d file_db
 
 -- 文件列表
 CREATE TABLE IF NOT EXISTS file_list (
@@ -21,6 +23,23 @@ COMMENT ON COLUMN file_list.tag_id IS '标签id';
 
 CREATE INDEX IF NOT EXISTS idx_file_id ON file_list(file_id);
 CREATE INDEX IF NOT EXISTS idx_tag_id ON file_list(tag_id);
+
+
+
+-- 标签管理
+CREATE TABLE IF NOT EXISTS tags (
+  id SERIAL PRIMARY KEY,
+  tag_id VARCHAR(255) NOT NULL,
+  tag_name VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+COMMENT ON COLUMN tags.tag_id IS '标签id';
+COMMENT ON COLUMN tags.tag_name IS '标签名称';
+COMMENT ON COLUMN tags.created_at IS '创建时间';
+COMMENT ON COLUMN tags.updated_at IS '更新时间';
+
+CREATE INDEX IF NOT EXISTS idx_tag_id ON tags(tag_id);
 
 
 
--- internal/model/entity/file_list.go
+++ internal/model/entity/file_list.go
@@ -10,14 +10,14 @@ import (
 
 // FileList is the golang structure for table file_list.
 type FileList struct {
-	Id            int         `json:"id"            orm:"id"              description:"主键"` //
-	FileId        string      `json:"fileId"        orm:"file_id"         description:"文件ID"` //
-	FileName      string      `json:"fileName"      orm:"file_name"       description:"文件名称"` //
-	FileSize      int64       `json:"fileSize"      orm:"file_size"       description:"文件大小"` //
-	FileType      string      `json:"fileType"      orm:"file_type"       description:"文件类型"` //
-	FileLocalPath string      `json:"fileLocalPath" orm:"file_local_path" description:"本地路径"` //
-	FileOssPath   string      `json:"fileOssPath"   orm:"file_oss_path"   description:"OSS路径"` //
-	CreatedAt     *gtime.Time `json:"createdAt"     orm:"created_at"      description:"创建时间"` //
-	UpdatedAt     *gtime.Time `json:"updatedAt"     orm:"updated_at"      description:"更新时间"` //
-	TagId         string      `json:"tagId"         orm:"tag_id"          description:"标签ID"` //
+	Id            int         `json:"id"            orm:"id"              description:""` //
+	FileId        string      `json:"fileId"        orm:"file_id"         description:""` //
+	FileName      string      `json:"fileName"      orm:"file_name"       description:""` //
+	FileSize      int64       `json:"fileSize"      orm:"file_size"       description:""` //
+	FileType      string      `json:"fileType"      orm:"file_type"       description:""` //
+	FileLocalPath string      `json:"fileLocalPath" orm:"file_local_path" description:""` //
+	FileOssPath   string      `json:"fileOssPath"   orm:"file_oss_path"   description:""` //
+	CreatedAt     *gtime.Time `json:"createdAt"     orm:"created_at"      description:""` //
+	UpdatedAt     *gtime.Time `json:"updatedAt"     orm:"updated_at"      description:""` //
+	TagId         string      `json:"tagId"         orm:"tag_id"          description:""` //
 }
