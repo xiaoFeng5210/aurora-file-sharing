caee7bcfeb70fbb3b792d5658e706679e6f088cb
Eadweard <aurora.coder.zqf@gmail.com>
2025-05-29 15:53:35 +0800

    feat: 开始构建新的file_list

--- internal/service/ftr_service.go
+++ internal/service/ftr_service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 
+	v1 "aurora-file-sharing/api/file_list/v1"
 	"aurora-file-sharing/internal/dao"
 
 	"github.com/gogf/gf/v2/database/gdb"
@@ -49,5 +50,19 @@ func UnbindFileTag(ctx context.Context, fileId string, tagId string) (err error)
 func GetAllTagsByFileId(ctx context.Context, fileId string) (res *gdb.Model) {
 	filesDao := dao.FileList.Ctx(ctx)
 	res = filesDao.InnerJoin("file_tag_relation", "ftr", "ftr.file_id = file_list.file_id").InnerJoin("tags", "tags.tag_id = ftr.tag_id").Fields("tags.*, file_list.file_id, file_list.file_name").Where("file_list.file_id = ?", fileId)
+	return
+}
+
+// 查询列表（带着拥有的标签数据）
+func GetFileListWithTags(ctx context.Context, data *v1.FileListReq) (res *gdb.Model) {
+	filesDao := dao.FileList.Ctx(ctx)
+	model := filesDao.InnerJoin("file_tag_relation", "ftr", "ftr.file_id = file_list.file_id").InnerJoin("tags", "tags.tag_id = ftr.tag_id")
+	if data.FileId != "" {
+		model = model.Where("file_list.file_id = ?", data.FileId)
+	}
+	if data.FileName != "" {
+		model = model.Where("file_list.file_name like ?", "%"+data.FileName+"%")
+	}
+	res = model.Limit(data.PageSize, (data.Page-1)*data.PageSize)
 	return
 }
