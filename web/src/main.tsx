d9c369b2998c7e2518117f63d1369b6482d4ad7e
Eadweard <aurora.coder.zqf@gmail.com>
2025-06-05 13:57:46 +0800

    fix all

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,7 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { StagewiseToolbar } from '@stagewise/toolbar-react';
 
 createRoot(document.getElementById('root')!).render(
-  <App />
+  <>
+    <App />
+    <StagewiseToolbar />
+  </>
 )
